60bb2c3a71f0a27613ce8af962878d8e27a3f8bc
aristo <dkfud2121@naver.com>
2022-09-04 14:55:55 +0900

    feat : setting git migration

--- mysite/account/models.py
+++ mysite/account/models.py
@@ -1,3 +1,20 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class user(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, null=False)  # PK
+
+    name = models.CharField(max_length=200, null=False)
+    email = models.EmailField(max_length=128, null=False)
+    password = models.CharField(max_length=200, null=False)
+
+    is_deleted = models.BooleanField(default=False, null=False)
+
+    # BaseEntity
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
+    updated_at = models.DateTimeField(auto_now=True, null=False)
+
+    class Meta:
+        db_table = 'user'
