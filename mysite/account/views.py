976dc035fb9e16e81bae16278445febb7e5e3d2b
aristo <dkfud2121@naver.com>
2022-09-04 16:19:02 +0900

    feat: add  post, get method test code

--- mysite/account/views.py
+++ mysite/account/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello_world(request):
-    return render(request, 'account/hello_world.html')
+
+    if request.method=="POST":
+        return render(request, 'account/hello_world.html', context={'text':'POST METHOD!!'})
+    else:
+            return render(request, 'account/hello_world.html', context={'text':'GET!!'})
+
