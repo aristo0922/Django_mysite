a9ec51df6a1d9372ddc1c921a805e5ad677f186c
aristo <dkfud2121@naver.com>
2022-09-04 17:28:57 +0900

    feat : HttpResponseRedirect, reverse와 반복문을 통한 입출력 관리

--- mysite/account/views.py
+++ mysite/account/views.py
@@ -1,11 +1,24 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+from account.models import user
+
+
 def hello_world(request):
 
     if request.method=="POST":
-        temp = request.POST.get('hello_world_input')
-        return render(request, 'account/hello_world.html', context={'text':temp})
+        input_name = request.POST.get('user_name')
+        input_email = request.POST.get('user_email')
+        input_password = request.POST.get('user_password')
+
+        new_user=user(name=input_name, email=input_email, password=input_password, is_deleted=False)
+        new_user.save()
+
+        return HttpResponseRedirect(reverse('account:hello_world'))
     else:
-            return render(request, 'account/hello_world.html', context={'text':'GET!!'})
+        new_user_list = user.objects.all()
+        return render(request, 'account/hello_world.html', context={'new_user_list':new_user_list})
 
