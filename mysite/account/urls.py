a0a2a4c329d982558704b175156a0f1f402233de
aristo <dkfud2121@naver.com>
2022-09-04 14:25:30 +0900

    feat: modified urls.py

--- mysite/account/urls.py
+++ mysite/account/urls.py
@@ -1,12 +1,7 @@
-from django.template.defaulttags import url
-from django.urls import include, path
-
-from account import admin
+from django.urls import path
 from account.views import hello_world
 
 app_name='account'
-
-
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world')
